4c9c3fae2f6410cb5a95ce7114d2f8718189e8e6
Urs Schulz <github@ursschulz.de>
2019-01-27 12:34:17 +0100

    first working version

---
+++ src/mac.rs
@@ -0,0 +1,84 @@
+// Copyright 2018 Urs Schulz
+//
+// This file is part of macchanger.
+//
+// macchanger is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// macchanger is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with macchanger.  If not, see <http://www.gnu.org/licenses/>.
+
+use std::fmt::Display;
+use std::fmt::Error;
+use std::fmt::Formatter;
+
+use libc::sockaddr;
+
+use std::io::Read;
+
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct MAC {
+    data: [u8; 6],
+}
+
+
+impl MAC {
+    pub fn new() -> Self {
+        Self { data: [0u8; 6] }
+    }
+
+    pub fn new_random() -> Self {
+        let mut out = Self::new();
+
+        // TODO: try different rngs here: urandom, hwrng, random, in this order
+        let mut f = std::fs::File::open("/dev/urandom").unwrap();
+        f.read_exact(&mut out.data).unwrap(); // then of course do not unwrap here...
+
+        out
+    }
+}
+
+
+impl From<sockaddr> for MAC {
+    fn from(addr: sockaddr) -> Self {
+        let mut out = Self::new();
+
+        for (n, x) in addr.sa_data[0..6].iter().enumerate() {
+            out.data[n] = *x as u8;
+        }
+
+        out
+    }
+}
+
+
+impl Into<[i8; 14]> for MAC {
+    fn into(self) -> [i8; 14] {
+        let mut out = [0i8; 14];
+
+        for (n, b) in self.data.iter().enumerate() {
+            out[n] = *b as i8;
+        }
+
+        out
+    }
+}
+
+
+impl Display for MAC {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        write!(
+            f,
+            "{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}",
+            self.data[0], self.data[1], self.data[2], self.data[3], self.data[4], self.data[5]
+        )
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,69 @@
+// Copyright 2018 Urs Schulz
+//
+// This file is part of macchanger.
+//
+// macchanger is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// macchanger is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with macchanger.  If not, see <http://www.gnu.org/licenses/>.
+
+mod mac;
+
+
+use netdevice::{get_hardware, set_hardware};
+
+use libc::c_int;
+
+use std::io::{Error, Result};
+
+use mac::MAC;
+
+
+/// Returns a new UDP socket
+fn get_socket() -> Result<c_int> {
+    use libc::{AF_INET, IPPROTO_UDP, SOCK_DGRAM};
+    let res = unsafe { libc::socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP) };
+
+    match res {
+        -1 => Err(Error::last_os_error()),
+        sock => Ok(sock),
+    }
+}
+
+
+fn get_mac(sock: c_int, ifname: &str) -> Result<MAC> {
+    let addr = get_hardware(sock, ifname)?;
+    Ok(addr.into())
+}
+
+
+fn set_mac(sock: c_int, ifname: &str, addr: &MAC) -> Result<()> {
+    let mut old_addr = get_hardware(sock, ifname)?;
+
+    old_addr.sa_data = addr.clone().into();
+    set_hardware(sock, ifname, old_addr)
+}
+
+
+fn main() -> Result<()> {
+    let ifname = std::env::args().nth(1).expect("No interface name given");
+
+    let sock = get_socket().expect("Failed to open socket");
+    let cur_addr = get_mac(sock, &ifname).expect("Failed to get hardware address");
+    let new_addr = MAC::new_random();
+
+    println!("Old address: {}", cur_addr);
+    println!("New address: {}", new_addr);
+
+    set_mac(sock, &ifname, &new_addr).expect("Failed to set new hardware address");
+
+    Ok(())
+}
