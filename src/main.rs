119e9a902f7d537956cd20c387c1e8dabe466658
Urs Schulz <github@ursschulz.de>
2019-01-29 15:11:11 +0100

    make CLI compatible to GNU macchanger

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,8 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with macchanger.  If not, see <http://www.gnu.org/licenses/>.
 
+#![feature(termination_trait_lib, process_exitcode_placeholder)]
+
 mod mac;
 
 
@@ -23,6 +25,9 @@ use netdevice::{get_hardware, set_hardware};
 use libc::c_int;
 
 use std::io::{Error, Result};
+use std::process::ExitCode;
+
+use colored::Colorize;
 
 use mac::MAC;
 
@@ -53,18 +58,130 @@ fn set_mac(sock: c_int, ifname: &str, addr: &MAC) -> Result<()> {
 }
 
 
+const MODES: [&str; 7] = [
+    "show",
+    "ending",
+    "another",
+    "any",
+    "permanent",
+    "random",
+    "mac",
+];
+
+fn conflicts(mode: &str) -> Vec<&str> {
+    let mut modes = MODES.to_vec();
+
+    modes.retain(|x| *x != mode);
+    modes
+}
+
+
 fn run() -> std::result::Result<(), String> {
-    let ifname = std::env::args()
-        .nth(1)
-        .ok_or("No interface name given".to_string())?;
+    use clap::{App, Arg};
+
+    let matches = App::new("macchanger")
+        .version("0.1")
+        .author("Urs Schulz <github@ursschulz.de>")
+        .about("Program to change MAC addresses")
+        .arg(
+            Arg::with_name("show")
+                .short("s")
+                .long("show")
+                .help("Print the MAC address and exit")
+                .conflicts_with_all(&conflicts("show")),
+        )
+        .arg(
+            Arg::with_name("ending")
+                .short("e")
+                .long("ending")
+                .help("Don't change the vendor bytes")
+                .conflicts_with_all(&conflicts("ending")),
+        )
+        .arg(
+            Arg::with_name("another")
+                .short("a")
+                .long("another")
+                .help("Set random vendor MAC of the same kind")
+                .conflicts_with_all(&conflicts("another")),
+        )
+        .arg(
+            Arg::with_name("any")
+                .short("A")
+                .long("any")
+                .help("Set random vendor MAC of any kind")
+                .conflicts_with_all(&conflicts("any")),
+        )
+        .arg(
+            Arg::with_name("permanent")
+                .short("p")
+                .long("permanent")
+                .help("Reset to original, permanent hardware MAC")
+                .conflicts_with_all(&conflicts("permanent")),
+        )
+        .arg(
+            Arg::with_name("random")
+                .short("r")
+                .long("random")
+                .help("Set fully random MAC")
+                .conflicts_with_all(&conflicts("random")),
+        )
+        .arg(
+            Arg::with_name("mac")
+                .short("m")
+                .long("mac")
+                .takes_value(true)
+                .value_name("XX:XX:XX:XX:XX:XX")
+                .require_equals(true)
+                .help("Set the MAC XX:XX:XX:XX:XX:XX")
+                .conflicts_with_all(&conflicts("mac")),
+        )
+        .arg(
+            Arg::with_name("bia")
+                .short("b")
+                .long("bia")
+                .help("Pretend to be a burned-in-address"),
+        )
+        .arg(
+            Arg::with_name("device")
+                .required(true)
+                .index(1)
+                .empty_values(false),
+        )
+        .get_matches();
+
+    let ifname = matches.value_of("device").unwrap();
+    let bia = matches.is_present("bia");
 
     let sock = get_socket().map_err(|e| format!("Failed to open socket: {}", e))?;
     let cur_addr =
         get_mac(sock, &ifname).map_err(|e| format!("Failed to get hardware address: {}", e))?;
-    let new_addr = MAC::new_random(true);
 
-    println!("Old address: {}", cur_addr);
-    println!("New address: {}", new_addr);
+    println!("Current MAC:   {}", cur_addr);
+    println!("Permanent MAC: {}", "TODO");
+
+    let new_addr = if matches.is_present("show") {
+        return Ok(());
+    } else if matches.is_present("ending") {
+        return Err("This option is currently not implemented.".to_string());
+    } else if matches.is_present("ending") {
+        return Err("This option is currently not implemented.".to_string());
+    } else if matches.is_present("another") {
+        return Err("This option is currently not implemented.".to_string());
+    } else if matches.is_present("any") {
+        return Err("This option is currently not implemented.".to_string());
+    } else if matches.is_present("permanent") {
+        return Err("This option is currently not implemented.".to_string());
+    } else if matches.is_present("random") {
+        MAC::new_random(bia)
+    } else if matches.is_present("mac") {
+        return Err("This option is currently not implemented.".to_string());
+    } else {
+        return Err(
+            "Exactly one of the following options is required:\n".to_string() + &MODES.join(", "),
+        );
+    };
+
+    println!("New MAC:       {}", new_addr);
 
     set_mac(sock, &ifname, &new_addr)
         .map_err(|e| format!("Failed to set new hardware address: {}", e))?;
@@ -73,10 +190,13 @@ fn run() -> std::result::Result<(), String> {
 }
 
 
-fn main() {
+fn main() -> ExitCode {
     let res = run();
     match res {
-        Ok(_) => {}
-        Err(e) => println!("{}", e),
-    };
+        Ok(_) => ExitCode::SUCCESS,
+        Err(e) => {
+            println!("[{}] {}", "ERROR".red(), e);
+            ExitCode::FAILURE
+        }
+    }
 }
